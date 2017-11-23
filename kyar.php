34deb19ec6f4fccdd0e3c58c8b67496502b404ce
HtetHtetAung <htethtetaung.htd24@gmail.com>
2017-11-23 14:28:02 +0630

    initial commit

---
+++ kyar.php
@@ -0,0 +1,38 @@
+<?php 
+
+for($a=1;$a<=8;$a++)
+{
+
+	for($b=1;$b<=8;$b++)
+	{
+		if($a%2!=0){
+
+
+		if($b%2!=0){
+			print"<img src='black.png'>";
+		}
+		else{
+			print"<img src='white.png'>";
+		}
+		}
+		else{
+			if($b%2!=0){
+			print"<img src='white.png'>";
+		}
+		else{
+			print"<img src='black.png'>";
+		}
+
+		}
+
+	}
+	print"<br>";
+
+
+}
+
+
+
+
+
+ ?>
